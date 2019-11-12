f0c42dd9632f8280ca11f3e8a91a82e7dc6016e1
Grucha8 <t.szamot1997@gmail.com>
2019-11-12 22:22:22 +0100

    Create ClientAddressProvider

---
+++ BlockchainAgents/src/main/java/blockchain/ethereumj/ClientAddressProvider.java
@@ -0,0 +1,28 @@
+package blockchain.ethereumj;
+
+import blockchain.utils.RemoteConnectionHandler;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ClientAddressProvider {
+
+    private static final String ADDRESSES_ENDPOINT = "localhost:5149/api/blockchain/clients";
+
+    public static Set<String> getHashes() {
+        String json = RemoteConnectionHandler.getServerAddressFromIpServer(ADDRESSES_ENDPOINT);
+
+        JsonArray asJsonArray = new JsonParser().parse(json).getAsJsonArray();
+
+        Set<String> addresses = new LinkedHashSet<>();
+        for (JsonElement jsonElement : asJsonArray) {
+            JsonElement hash = jsonElement.getAsJsonObject().get("hash");
+            addresses.add(hash.getAsString());
+        }
+
+        return addresses;
+    }
+}
